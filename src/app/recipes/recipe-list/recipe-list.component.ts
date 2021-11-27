6e7f3b8384018dae4a74d5f976d8c4b3b33f0454
Constantin Mirea <58054095+ConstantinMirea@users.noreply.github.com>
2021-11-27 17:11:19 +0000

    commit 27/1

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
  recipes:Recipe[]=[
-   new Recipe('A test recipe', 'this is a test', 'link')
-
+   new Recipe('A test recipe', 'this is a test', 'https://www.simplyrecipes.com/thmb/JM-orIwOXTKXsUodT8gToU06w40=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-LEAD-3-8aa37af554cf4445a1e3e54fdc66e974.jpg'),
+   new Recipe('A test recipe vol 2', 'this is a test vol 2', 'https://www.simplyrecipes.com/thmb/JM-orIwOXTKXsUodT8gToU06w40=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-LEAD-3-8aa37af554cf4445a1e3e54fdc66e974.jpg')
  ];
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
